ea17e43245a635644e248e81e8ad3714621feb93
Mohamed Ramadan <114493793+GafaRamadan@users.noreply.github.com>
2023-02-21 23:36:55 +0200

    Add files via upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,33 @@
         </div>
         <div class="container w-full px-5 py-6 mx-auto">
             <div class="grid lg:grid-cols-4 gap-y-6">
-                
+                <div class="container w-full px-5 py-6 mx-auto">
+                    <div class="grid lg:grid-cols-4 gap-y-6">
+                        @if($specials != null)
+                        @foreach ($specials->menus as $menu)
+                            <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+                                <img class="w-full h-48" rc="{{ Storage::url($category->image) }}" alt="Image" />
+                                <div class="px-6 py-4">
+                                    <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">
+                                        {{ $menu->name }}</h4>
+                                    <p class="leading-normal text-gray-700">{{ $menu->description }}.</p>
+                                </div>
+                                <div class="flex items-center justify-between p-4">
+                                    <span class="text-xl text-green-600">${{ $menu->price }}</span>
+                                </div>
+                            </div>
+                        @endforeach
+                        @else
+                    </div>
+                    </div>
+                    <section class="mt-8 bg-white mb-5">
+                <div class="mt-4 text-center">
+                <p class="text-2xl font-bold " style="color:green;margin-bottom:50px">no special meal now</p>
+                      
+                        @endif
+                </div>
+                    </section>
+            </section>
             </div>
         </div>
     </section>
